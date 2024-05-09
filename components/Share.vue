f79d07a009d7238656194def86d5ec59a1c7bfce
Erico <erico@tapronto.com.br>
2024-05-09 19:20:43 -0300

    Link de share

--- components/Share.vue
+++ components/Share.vue
@@ -1,11 +1,7 @@
-<script setup>
-	const TwitterShareUrl = computed(()=>'https://twitter.com/intent/tweet?url=' + encodeURIComponent(useRequestURL().href));
-	const FacebookShareUrl = computed(()=>'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(useRequestURL().href));
-</script>
 <template>
 	<div class="share">
 	  <span>Compartilhe</span>
-	  <a class="twitter" :href="TwitterShareUrl" target="_blank"></a>
-	  <a class="facebook" :href="FacebookShareUrl" target="_blank"></a>
+	  <a class="twitter" onclick="window.open('https://twitter.com/intent/tweet?url='+encodeURIComponent(window.location.href),'_blank')"></a>
+	  <a class="facebook" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(window.location.href),'_blank')"></a>
 	</div>
 </template>
